53948c8d77e5d08949f73296238303bb9c8da798
YuanYouYuan <az6980522@gmail.com>
2017-07-07 13:08:33 +0800

    add speech recognition

--- weather-test.py
+++ weather-test.py
@@ -1,27 +1,38 @@
-# This version is for windows, and there must be a mpv application in this work directory
-
+import speech_recognition as sr
 import forecastio
 from gtts import gTTS
 import subprocess
 
 api_key = 'Enter your api key'
-
-# the following coordinate is Taipei/Taiwan
 lat = 25.0391667
 lng =  121.525
 lang = 'zh-TW'
 file_name = 'weather.mp3'
-player = './mpv'
+player = 'mpv'
 
 forecast = forecastio.load_forecast(api_key, lat, lng)
+r = sr.Recognizer()
 
-by_hour = forecast.hourly()
-
-for data in by_hour.data:
-    text = '在' + str(data.time) + '氣溫是' +  str(data.temperature) + '度西'
+def speak(text):
     print(text)
     tts = gTTS(text, lang)
     tts.save(file_name)
     subprocess.call([player, file_name])
-    
 
+
+while True:
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+        cmd = r.recognize_google(audio, language = lang)
+        print(cmd)
+
+    if cmd == '你好':
+        speak('你好啊，我是拿鐵熊貓機器人，你可以問我有關天氣的問題喔!')
+
+    elif cmd == '氣溫預報':
+        by_hour = forecast.hourly()
+        for data in by_hour.data:
+            speak('在' + str(data.time) + '氣溫是' +  str(data.temperature) + '度西')
+    else:
+        speak("對不起，我不懂你在說什麼")
